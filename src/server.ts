8a5ca28604f58e3eccbb5284b7ab59dc6084a576
shantunu007 <chakmashantunu.web@gmail.com>
2025-12-01 11:04:55 +0600

    Implemented POST / route to receive JSON body and return success response

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,31 @@
 import express, { Request, Response } from "express";
+import {Pool} from "pg";
+
 const app = express()
 const port = 5000;
+
+const pool = new Pool({
+    connectionString: `postgresql://neondb_owner:npg_4SzZJG7gbqVk@ep-winter-sun-ahjppj6d-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+});
+
+// parser
+app.use(express.json()); //middleware
+// app.use(express.urlencoded());
+
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello boss')
 });
+
+app.post("/", (req: Request, res: Response) => {
+    console.log(req.body);
+
+    res.status(201).json({
+        success: true,
+        message: "API is working"
+    })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
