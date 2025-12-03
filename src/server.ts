e72fdd2014437bd9dd789b78c8f5308d7269e0fe
shantunu007 <chakmashantunu.web@gmail.com>
2025-12-03 12:52:34 +0600

    Implemented update route with dynamic user ID

--- src/server.ts
+++ src/server.ts
@@ -96,6 +96,7 @@ app.get("/users", async (req: Request, res: Response) => {
     }
 });
 
+// dynamic user
 app.get("/users/:id", async (req: Request, res: Response) => {
     try {
         const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [req.params.id]);
@@ -109,6 +110,33 @@ app.get("/users/:id", async (req: Request, res: Response) => {
             res.status(200).json({
                 success: true,
                 message: "User fetched successfully",
+                data: result.rows[0]
+            })
+        }
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err
+        })
+    }
+});
+
+
+app.put("/users/:id", async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    try {
+        const result = await pool.query(`UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *`, [name, email, req.params.id]);
+
+        if (result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        } else {
+            res.status(200).json({
+                success: true,
+                message: "User updated successfully",
                 data: result.rows[0]
             })
         }
