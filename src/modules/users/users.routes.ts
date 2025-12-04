2d29171962469d14e37e8dbf356a0b98231f4dd6
shantunu007 <chakmashantunu.web@gmail.com>
2025-12-04 14:06:06 +0600

    extracted user creation logic into createUser controller

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,30 +1,11 @@
 import express, { Request, Response } from "express";
 import { pool } from "../../config/db";
+import { usersControllers } from "./users.controller";
 
 const router = express.Router();
 
 // app.use("/users", userRoutes)
-router.post("/", async (req: Request, res: Response) => {
-    console.log(req.body);
-    const { name, email } = req.body;
-
-    try {
-        const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email]);
-        // console.log(result.rows[0]);
-        return res.status(201).json({
-            success: true,
-            message: "Data inserted Successfully",
-            data: result.rows[0],
-        });
-
-        // res.send({ message: "data inserted" })
-    } catch (err: any) {
-        return res.status(500).json({
-            success: false,
-            message: err.message
-        });
-    }
-});
+router.post("/", usersControllers.createUser);
 
 
 router.get("/", async (req: Request, res: Response) => {
